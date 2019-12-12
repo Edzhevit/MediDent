e710058190bd8168b1fd7ba494efc7ab2a8561a3
Edzhevit <nightwalker_@abv.bg>
2019-12-12 12:08:03 +0200

    Added cloudinary

---
+++ src/main/java/softuni/medident/service/services/CloudinaryService.java
@@ -0,0 +1,10 @@
+package softuni.medident.service.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface CloudinaryService {
+
+    String upload(MultipartFile multipartFile) throws IOException;
+}
