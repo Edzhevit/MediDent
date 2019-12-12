6cc07233e472441dbf6498cd2fe48d634cca6d47
Edzhevit <nightwalker_@abv.bg>
2019-12-12 12:09:16 +0200

    Added constants and exception handler

---
+++ src/main/java/softuni/medident/constants/Constants.java
@@ -0,0 +1,19 @@
+package softuni.medident.constants;
+
+import java.math.BigDecimal;
+
+public class Constants {
+
+    public final static String DEFAULT_ID = "12345";
+    public final static String DEFAULT_NAME = "Pesho";
+    public final static String DEFAULT_LAST_NAME = "Peshov";
+    public final static String DEFAULT_PASSWORD = "123QWEqwe_~#";
+    public final static String PASSWORD_NOT_MATCH = "321qweQWE_~#";
+    public final static String DEFAULT_EMAIL = "valid@email.bg";
+    public final static String DEFAULT_PHONE_NUMBER = "0888112233";
+    public final static Integer DEFAULT_AGE = 25;
+    public final static String DEFAULT_LOCATION = "Sofia";
+    public final static String DEFAULT_DESCRIPTION = "Valid Description";
+    public final static BigDecimal DEFAULT_SALARY = BigDecimal.valueOf(2000);
+
+}
---
+++ src/main/java/softuni/medident/exception/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package softuni.medident.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No such user")
+public class UserNotFoundException extends Exception {
+
+    public UserNotFoundException(String message){
+        super(message);
+    }
+}
