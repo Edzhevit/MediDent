4ded1510c06ab89403700a2cfeb0b2b3228fbfa4
Edzhevit <nightwalker_@abv.bg>
2019-12-18 20:32:05 +0200

    Added constants

---
+++ src/main/java/softuni/medident/constants/CloudinaryConstants.java
@@ -0,0 +1,8 @@
+package softuni.medident.constants;
+
+public class CloudinaryConstants {
+
+    public final static String CLOUD_NAME = "cloud_name";
+    public final static String API_KEY = "api_key";
+    public final static String API_SECRET = "api_secret";
+}
---
+++ src/main/java/softuni/medident/constants/RoleConstants.java
@@ -0,0 +1,9 @@
+package softuni.medident.constants;
+
+public class RoleConstants {
+
+    public final static String ROOT_ROLE = "ROOT";
+    public final static String ADMIN_ROLE = "ADMIN";
+    public final static String USER_ROLE = "USER";
+
+}
