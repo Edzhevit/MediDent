42b0016f2201a7abef652801d02bb2c0c58bac04
Edzhevit <nightwalker_@abv.bg>
2019-12-18 20:35:52 +0200

    Added edit profile and upload photo for user

---
+++ src/main/java/softuni/medident/web/models/UserEditModel.java
@@ -0,0 +1,18 @@
+package softuni.medident.web.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+import softuni.medident.service.models.base.BaseModel;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserEditModel extends BaseModel {
+
+    private Integer age;
+    private String phoneNumber;
+    private MultipartFile image;
+
+}
